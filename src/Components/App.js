ec0ea00e60165de5ee89b45e7701ac226571619f
heshamamoudi <hesham69mez@gmail.com>
2021-02-09 21:06:15 +0300

    lastupdate

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,38 +1,48 @@
 import React from "react";
 import Signup from "./Signup";
 import Login from "./Login";
-import CompleteDetails from "./CompleteDetails"
+import CompleteDetails from "./CompleteDetails";
 import Dashboard from "./Dashboard";
-import PrivateRoute from './PrivateRoute'
-import ForgotPassword from './ForgotPassword'
-import UpdateProfile from './UpdateProfile'
+import PrivateRoute from "./PrivateRoute";
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../Context/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <PrivateRoute path="/complete-details" component={CompleteDetails} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/ezb" component={Login} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
+    <div>
+      <Router>
+        <AuthProvider>
+          <Switch>
+              <PrivateRoute exact path="/" component={NavBar,Dashboard} />
+            <Container
+              className="d-flex align-items-center justify-content-center"
+              style={{ minHeight: "100vh" }}
+            >
+              <div className="w-100" style={{ maxWidth: "400px" }}>
+                <PrivateRoute
+                  path="/update-profile"
+                  component={UpdateProfile}
+                />
+                <PrivateRoute
+                  path="/complete-details"
+                  component={CompleteDetails}
+                />
+                <Route path="/signup" component={Signup} >
+                  
+                </Route>
+                <Route path="/ezb" component={Login} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </div>
+            </Container>
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 }
 
