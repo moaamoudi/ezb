bf69ce6783a87cb188c299e290fbe6fb19f9a0a3
heshamamoudi <hesham69mez@gmail.com>
2021-02-23 16:16:14 +0300

    tabs done

--- src/Components/ProjectPage.js
+++ src/Components/ProjectPage.js
@@ -1,12 +1,37 @@
 import React from "react";
 import { useAuth } from "../Context/AuthContext";
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "./styles/tabs.css";
 export default function ProjectPage() {
   const { project } = useAuth();
 
   return (
-    <div style={{ width: "100%", textAlign: "center" }}>
+    <div style={{ width: "100%", textAlign: "left" }}>
       <h1>{project.projectName}</h1>
+      <div>
+        <Tabs >
+          <TabList>
+            <Tab>Current Work</Tab>
+            <Tab>Gantt Chart</Tab>
+            <Tab>NPV</Tab>
+            <Tab>Inventory</Tab>
+          </TabList>
+          <div>
+            <TabPanel>
+              <div style={{ width: "100%", height: "100%" }}>hello</div>
+            </TabPanel>
+            <TabPanel>
+              <h2>Any content 2</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Any content 3</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Any content 4</h2>
+            </TabPanel>
+          </div>
+        </Tabs>
+      </div>
     </div>
   );
 }
