8d7ce84498c1d0c32be2786e2192565a03b23d78
heshamamoudi <hesham69mez@gmail.com>
2021-02-25 14:03:08 +0300

    npm being edited

--- src/Components/ProjectPage.js
+++ src/Components/ProjectPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth } from "../Context/AuthContext";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { Form, Button} from "react-bootstrap";
 import "./styles/tabs.css";
 export default function ProjectPage() {
   const { project } = useAuth();
@@ -16,7 +17,7 @@ export default function ProjectPage() {
             <Tab>NPV</Tab>
             <Tab>Inventory</Tab>
           </TabList>
-          <div>
+          <div >
             <TabPanel>
               <div style={{ width: "100%", height: "100%" }}>hello</div>
             </TabPanel>
@@ -24,7 +25,49 @@ export default function ProjectPage() {
               <h2>Any content 2</h2>
             </TabPanel>
             <TabPanel>
-              <h2>Any content 3</h2>
+              <div style={{ width: "100%", height: "100%" }} >
+                
+              <Form >
+              <Form.Group id="ProjectName">
+                <Form.Label>Initial Investment</Form.Label>
+                
+                <Form.Control
+                style={{ width: "20%"  }}
+                  type="Name"
+                  // ref={ProjectName}
+                  icon="fas fa-dollar-sign"
+                  required
+                  className="form-control button-bg "
+                />
+                
+              </Form.Group>
+
+              <Form.Group id="ProjectDescription">
+                <Form.Label>Project Description:</Form.Label>
+                <Form.Control
+                  type="Name"
+                  // ref={ProjectDescription}
+                  required
+                  className="form-control button-bg"
+                />
+              </Form.Group>
+
+              <div className="text-center">
+                <Button
+                  variant="light"
+                  className="w-50 button-bg mt-3"
+                  type="submit"
+                >
+                  Calculate
+                </Button>
+                
+              </div>
+              
+              
+            </Form>
+
+
+              </div>
             </TabPanel>
             <TabPanel>
               <h2>Any content 4</h2>
