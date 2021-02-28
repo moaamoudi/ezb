5abfa7a967d8b929dcb3a9a1fadb98df6e696f9d
heshamamoudi <hesham69mez@gmail.com>
2021-02-28 20:15:16 +0300

    updated clients page with all layout
    needs functionality and css

--- src/Components/ClientsPage.js
+++ src/Components/ClientsPage.js
@@ -1,93 +1,71 @@
 import React from "react";
-import { Paragraph } from 'react-bootstrap-icons'
-import './ClientsPage.css'
-import BootstrapTable from 'react-bootstrap-table-next';
-import paginationFactory from 'react-bootstrap-table2-paginator';
-import {Card,Button} from "react-bootstrap";
-
-
-
-
+import { Paragraph } from "react-bootstrap-icons";
+import "./styles/ClientsPage.css";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import { Card, Button } from "react-bootstrap";
 
 export default function ClientsPage() {
-   
+  const columns = [
+    {
+      dataField: "name",
+      text: "Client Name",
+    },
+    {
+      dataField: "Email",
+      text: "Email",
+    },
+  ];
 
-   
-    
-    const columns = [{
+  const data = [
+    {
+      name: "ana anan",
+      Email: "asdasd",
+    },
+    {
+      name: "saden mg",
+      Email: "Test@outlook.com",
+    },
+  ];
 
-        dataField: 'name',
-        text: 'Client Name'
-        
-        
-      }, {
-        dataField: 'Email',
-        text: 'Email'
-      }];
+  const col = [
+    {
+      dataField: "name",
+      text: "Employee Name",
+    },
+    {
+      dataField: "Email",
+      text: "Email",
+    },
+  ];
 
-      const data = [{
-        name : "ana anan",
-        Email : "asdasd",
-      
-        }, 
-        {
-          name: "saden mg",
-          Email: "Test@outlook.com",
-          
-        }
-      
-      ];
+  const selectRow = {
+    mode: "checkbox",
+    clickToSelect: true,
+    style: { backgroundColor: "#f5a494" },
+  };
 
+  const data2 = [
+    {
+      name: "ana anan",
+      Email: "asdasd",
+    },
+    {
+      name: "saden mg",
+      Email: "Test@outlook.com",
+    },
+  ];
 
-        const col = [{
+  return (
+    <div>
+      <div className="main">
+        <div className="test" style={{ height: "fit-content" }}>
+          <Button className="Btn mb-2">Add</Button>
+          <Button className="mb-2"> delete</Button>
 
-            dataField: 'name',
-            text: 'Employee Name'
-          }, {
-            dataField: 'Email',
-            text: 'Email'
-          }];
-
-          const selectRow = { 
-            mode: 'checkbox',
-            clickToSelect: true,
-            style: { backgroundColor: '#f5a494' }
-          };
-
-            
-          const data2 = [{
-            name : "ana anan",
-            Email : "asdasd",
-            }, 
-            {
-              name: "saden mg",
-              Email: "Test@outlook.com",
-            }
-          
-          ];
-    
-    return (
-      <div>
-        <div className="main"
-        
-        > 
-        <div className="empButton">
-            <Button className ="mr-5">delete</Button> 
-               <Button>delete</Button> 
-               
-
-               
-               </div>
-        
-        
-          <div className="test" style={{height:"fit-content"}}>
-         
-            <Card className="main-shadow"
-     
-            >
-              
+          <div>
+            <Card className="main-shadow">
               <Card.Body>
-
                 <BootstrapTable
                   keyField="Email"
                   data={data2}
@@ -98,8 +76,14 @@ export default function ClientsPage() {
               </Card.Body>
             </Card>
           </div>
-          <div className="space"> </div>
-          <div className="test" style={{height:"fit-content"}} >
+          <Button className="mt-2 ml-1"> Email</Button>
+        </div>
+
+        <div className="space"> </div>
+        <div className="test" style={{ height: "fit-content" }}>
+          <Button className="Btn mb-2">Add</Button>
+          <Button className="mb-2"> delete</Button>
+          <div>
             <Card className="main-shadow">
               <Card.Body>
                 <BootstrapTable
@@ -112,12 +96,9 @@ export default function ClientsPage() {
               </Card.Body>
             </Card>
           </div>
+          <Button className="mt-2 ml-1"> Email</Button>
         </div>
       </div>
-    );
-
-
-    
-
-    
+    </div>
+  );
 }
