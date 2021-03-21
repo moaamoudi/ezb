0b057a1516f089c5ebfcd95b7c5f46d457e7cada
mohamoudi <78143032+moaamoudi@users.noreply.github.com>
2021-03-21 21:37:06 +0300

    lineChart done

--- src/Components/Tabs/GanttChart.js
+++ src/Components/Tabs/GanttChart.js
@@ -67,63 +67,6 @@ export default function GanttChart() {
     return (counter / task.subTasks.length) * 100;
   }
 
-  // [
-  //   [
-  //     { type: "string", label: "Task ID" },
-  //     { type: "string", label: "Task Name" },
-  //     { type: "date", label: "Start Date" },
-  //     { type: "date", label: "End Date" },
-  //     { type: "number", label: "Duration" },
-  //     { type: "number", label: "Percent Complete" },
-  //     { type: "string", label: "Dependencies" },
-  //   ],
-  //   [
-  //     "Research",
-  //     "Find sources",
-  //     new Date(2015, 0, 1),
-  //     new Date(2015, 0, 5),
-  //     null,
-  //     100,
-  //     null,
-  //   ],
-  //   [
-  //     "Write",
-  //     "Write paper",
-  //     null,
-  //     new Date(2015, 0, 9),
-  //     3 * 24 * 60 * 60 * 1000,
-  //     25,
-  //     "Research,Outline",
-  //   ],
-  //   [
-  //     "Cite",
-  //     "Create bibliography",
-  //     null,
-  //     new Date(2015, 0, 7),
-  //     1 * 24 * 60 * 60 * 1000,
-  //     20,
-  //     "Research",
-  //   ],
-  //   [
-  //     "Complete",
-  //     "Hand in paper",
-  //     null,
-  //     new Date(2015, 0, 10),
-  //     1 * 24 * 60 * 60 * 1000,
-  //     0,
-  //     "Cite,Write",
-  //   ],
-  //   [
-  //     "Outline",
-  //     "Outline paper",
-  //     null,
-  //     new Date(2015, 0, 6),
-  //     1 * 24 * 60 * 60 * 1000,
-  //     100,
-  //     "Research",
-  //   ],
-  // ]
-
   return (
     <div style={{ width: "100%", padding: "50px" }}>
       <Chart
