0f4c08c203ab1fa0c2c24c92d27e38a78128f454
heshamamoudi <hesham69mez@gmail.com>
2021-03-30 02:57:18 +0300

    completed assigned details view for worker

--- src/Components/PopUpEmpDetailsAssigned.js
+++ src/Components/PopUpEmpDetailsAssigned.js
@@ -10,38 +10,89 @@ export default function PopUpEmpDetailsAssigned(props) {
   const [Emp] = useState(props.Emp);
   const { selectedProjectTasks } = useAuth();
   const [tasks, setTasks] = useState([]);
-  //   this will be implemented when the assigned is adjusted! dont remove!!!!
-  // useEffect(() => {
-  //    for (let index = 0; index < selectedProjectTasks.length; index++) {
-  //       for (let j = 0; j < selectedProjectTasks.assigned.length; j++) {
-  //           if(selectedProjectTasks.assigned[j].email===Emp.email){
-  //               if(tasks.length<1){
-  //                 setTasks({name:selectedProjectTasks.assigned[j].subTaskName,complete:selectedProjectTasks.assigned[j].complete})
-  //               }
-  //           }
 
-  //       }}
-  // }, [])
+  useEffect(() => {
+    for (let index = 0; index < selectedProjectTasks.length; index++) {
+      for (let j = 0; j < selectedProjectTasks[index].subTasks.length; j++) {
+        if (selectedProjectTasks[index].subTasks[j].assigned !== undefined) {
+          if (
+            selectedProjectTasks[index].subTasks[j].assigned.email === Emp.email
+          ) {
+            if (tasks.length < 1) {
+              var taskObj = {
+                name: selectedProjectTasks[index].subTasks[j].name,
+                complete: selectedProjectTasks[index].subTasks[j].complete,
+                completeDate:
+                  selectedProjectTasks[index].subTasks[j].completionDate,
+                majorTask: selectedProjectTasks[index].taskName,
+              };
+              setTasks(tasks.concat(taskObj));
+            } else {
+              var found = -1;
+              var taskObj = {
+                name: selectedProjectTasks[index].subTasks[j].name,
+                complete: selectedProjectTasks[index].subTasks[j].complete,
+                completeDate:
+                  selectedProjectTasks[index].subTasks[j].completionDate,
+                majorTask: selectedProjectTasks[index].taskName,
+              };
+              for (let dublicate = 0; dublicate < tasks.length; dublicate++) {
+                if (
+                  tasks[dublicate].majorTask === taskObj.majorTask &&
+                  tasks[dublicate].name === taskObj.name
+                ) {
+                  found = 1;
+                }
+              }
+              if (found === -1) {
+                var joined = tasks.concat(taskObj);
+                setTasks(joined);
+              }
+            }
+          }
+        }
+      }
+    }
+  }, []);
 
   return (
     <Popup
-      trigger={<Button style={{margin:'0'}}>Details</Button>}
+      trigger={<Button style={{ margin: "0" }}>Details</Button>}
       position="center center"
       modal
       nested
     >
       {(close) => (
-        <Card className="main-shadow" style={{ width: "400px" }}>
+        <Card
+          className="main-shadow"
+          style={{ width: "500px", height: "600px" }}
+        >
           <Card.Body>
-             
             <h2 className="text-center mb-4">Employee Details</h2>
-             <div><h4>
-              Employee Name:{Emp.name}
-            </h4></div>
-            <div><h4>Employee Email:{Emp.email}</h4></div>
-            {Emp.type==='Administrator'?(<></>):(<div><h5>Assigned Tasks:</h5></div>)}
-            
-            {/* {mapping of tasks} */}
+            <div>
+              <h4>Employee Name:{Emp.name}</h4>
+            </div>
+            <div>
+              <h4>Employee Email:{Emp.email}</h4>
+            </div>
+            {Emp.type !== "Administrator" ? (
+              <div>
+                <h5>Assigned Tasks:</h5>
+                {tasks.length !== 0 ? (
+                  tasks.map((task) => (
+                    <div>
+                      {console.log(tasks)}
+                      {task.name}
+                    </div>
+                  ))
+                ) : (
+                  <div>this employee is not assigned to any task</div>
+                )}
+              </div>
+            ) : (
+              <div></div>
+            )}
+
             <div className="text-center">
               <Button
                 className="w-50  mt-3"
